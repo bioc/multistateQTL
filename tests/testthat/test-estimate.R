d3e48eb70dbf63d98d077fc656988f9be64bb428
dunstone-a <amelia.dunstone@gmail.com>
2025-03-12 22:47:33 +1100

    Test estimate parameters

--- tests/testthat/test-estimate.R
+++ tests/testthat/test-estimate.R
@@ -3,23 +3,17 @@
 # source("setup.R"); source("test-estimate.R")
 
 test_that("estimate parameters works for gtex data", {
-    
-    # Parameters used for the simulation
-    params <- list(
-        betas.sig.shape = 2.160473,
-        betas.sig.rate = 14.19137,
-        cv.sig.shape = 16.6759,
-        cv.sig.rate = 35.56338,
-        betas.null.shape = 1.053135,
-        betas.null.rate = 22.55316,
-        cv.null.shape = 1.698207,
-        cv.null.rate = 0.7553863)
-    
-    qtleSimulate
 
     params <- qtleEstimate(gtex, threshSig = 0.05, threshNull = 0.5)
     params
 
     expect_equal(round(params$cv.sig.shape, 2), 7.07)
+    expect_equal(round(params$cv.sig.rate, 2), 7.72)
+    expect_equal(round(params$cv.null.shape, 2), 1.90)
+    expect_equal(round(params$cv.null.rate, 2), 0.90)
+    expect_equal(round(params$betas.sig.shape, 2), 3.47)
+    expect_equal(round(params$betas.sig.rate, 2), 16.27)
+    expect_equal(round(params$betas.null.shape, 2), 1.36)
+    expect_equal(round(params$betas.null.rate, 2), 11.03)
 })
 
